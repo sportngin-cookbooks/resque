a71126b97a90e1b74a9db122447105a30c4e151a
Andy Fleener <andrew.fleener@sportngin.com>
2014-12-05 11:15:06 -0600

    Switch to old school init.d

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,12 @@
 node.override[:resque][:exec_prefix] = "exec bundle exec" if node[:resque][:use_bundle_exec]
 
+template "/etc/init.d/resque" do
+  source "resque-init.sh.erb"
+  cookbook "resque"
+  mode "0644"
+  variables node[:resque]
+end
+
 template "/etc/init/resque.conf" do
   source "resque.upstart.erb"
   cookbook "resque"
