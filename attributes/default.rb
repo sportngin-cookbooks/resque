c992dc0e831e7bff5cc0a514cc0d6c156d4d46a9
Andy Fleener <andrew.fleener@sportngin.com>
2014-12-08 12:04:23 -0600

    Fix 2 minor bugs with the init script

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,11 @@
-default[:resque][:exec_prefix] = 'bundle exec'
+default[:resque][:exec_prefix] = 'exec bundle exec'
 default[:resque][:environment] = 'production'
 default[:resque][:config_file] = 'config/resque.yml'
 default[:resque][:pids_dir] = 'tmp/pids'
 default[:resque][:logfile] = 'log/resque_workers.log'
 default[:resque][:resque_conf] = '/etc/resque.conf'
 default[:resque][:bin_file] = '/usr/local/bin/resque'
-default[:resque][:rake] = 'rake'
+default[:resque][:rake_bin] = 'rake'
 default[:resque][:rake_task] = 'resque:work'
 default[:resque][:user] = 'root'
 default[:resque][:env_vars] = { 'PATH' => '/usr/local/bin:$PATH' }
